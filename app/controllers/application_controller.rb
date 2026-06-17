82ee557ad7e45a6b8375585bf1991d3bf1bcbfa4
KayJay-26 <khrithikjupalli@gmail.com>
2026-06-17 12:04:01 +0530

    Reorder protected keyword placement in ApplicationController for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
   def seed
   load Rails.root.join("db/seeds.rb")
   render plain: "Database seeded!"
   end
 
+
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
