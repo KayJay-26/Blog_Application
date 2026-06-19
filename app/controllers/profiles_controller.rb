201cb01c841ed0a62d0691c695bb22d7e28f0608
KayJay-26 <khrithikjupalli@gmail.com>
2026-06-19 15:17:40 +0530

    Remove unnecessary blank lines in ProfilesController show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,7 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
-
     @posts = @user.posts.order(created_at: :desc)
-
     @comments = @user.comments.order(created_at: :desc)
   end
 end
