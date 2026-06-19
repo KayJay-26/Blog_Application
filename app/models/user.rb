97f836b465b4a2f809c65a10fa2f81c2b9aa1e91
KayJay-26 <khrithikjupalli@gmail.com>
2026-06-19 11:25:06 +0530

    Refactor devise modules formatting in User model for improved readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-        
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 end
